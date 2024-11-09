5517a995cf4577b012395987f470b21812730ccd
Mouni2695 <kalimounika02@gmail.com>
2024-11-09 11:38:58 +0530

    first commit

---
+++ JavaScript/ext.js
@@ -0,0 +1,6 @@
+function Add(){
+    document.frm.t3.value=parseInt(document.frm.t1.value)+parseInt(document.frm.t2.value);
+}
+function Multiply(){
+    document.frm.t3.value=parseInt(document.frm.t1.value)*parseInt(document.frm.t2.value);
+}
